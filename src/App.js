9185b0839d96ff4fea727268f3804a5e2df5d723
Curtis Rutland <crutland@amherst.com>
2019-03-04 13:12:33 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Content from "./Content";
+
+function App() {
+
+
+
+  return (
+    <>
+      <Navbar bg="info" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">The Clap</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container style={{marginTop: 20}}>
+        <Row>
+          <Col>
+            <Content />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+
+export default App;
---
+++ src/Content.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import options from "./options.json";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+export default function Content() {
+    const [token, setToken] = React.useState(options[0].value);
+    const [input, setInput] = React.useState("");
+    const [buttonTxt, setButtonTxt] = React.useState("Copy to Clipboard");
+
+    const outputRef = React.useRef(null);
+    const inputRef = React.useRef(null);
+
+    React.useEffect(() => inputRef.current.focus());
+
+    const handleSetToken = (val) => () => {
+        setToken(val);
+    }
+
+    const opts = options.map((o, i) => <Dropdown.Item onClick={handleSetToken(o.value)} key={i}>{o.display}</Dropdown.Item>)
+
+    function handleInputChange(evt) {
+        setInput(evt.target.value);
+    }
+
+    function generateOutput() {
+        const inputTokens = input.trim().split(' ').filter(t => t.trim() !== "");
+        let results = [];
+        for (let it of inputTokens) {
+            results.push(it);
+            results.push(token);
+        };
+        const out = results.join(' ');
+        return out;
+    }
+
+    function copy(evt) {
+        outputRef.current.select();
+        document.execCommand("copy");
+        evt.target.focus();
+        setButtonTxt("Copied!");
+        setTimeout(() => setButtonTxt("Copy to Clipboard"), 3000);
+    }
+
+    const output = generateOutput();
+
+    return (
+        <Form>
+            <Form.Group>
+                <Form.Label>Input</Form.Label>
+                <Form.Control type="text" placeholder="Enter phrase..." value={input} onChange={handleInputChange} ref={inputRef} />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Separator</Form.Label>
+                <InputGroup>
+                    <FormControl placeholder="Text to insert..." aria-label="Text to insert..." value={token} onChange={e => setToken(e.target.value)} />
+                    <DropdownButton as={InputGroup.Append} variant="outline-secondary" title="Presets" id="input-group-dropdown-2">
+                        {opts}
+                    </DropdownButton>
+                </InputGroup>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Results</Form.Label>
+                <InputGroup>
+                    <FormControl readOnly placeholder="Output goes here..." value={output} ref={outputRef} />
+                    <InputGroup.Append>
+                        <Button variant="outline-secondary" onClick={copy}>{buttonTxt}</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+            </Form.Group>
+        </Form>
+    )
+}
