d7a2d9af9cacea697e8c0ff677af5a94856cd42a
Curtis Rutland <crutland@amherst.com>
2019-03-04 13:06:33 -0600

    Updates

---
+++ precache-manifest.a8cbf13758bf11ff4ff5171daefebc4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "392eb6f68055726add5e",
+    "url": "/the-clap/static/js/runtime~main.392eb6f6.js"
+  },
+  {
+    "revision": "66a1595ab2853de4fc22",
+    "url": "/the-clap/static/js/main.66a1595a.chunk.js"
+  },
+  {
+    "revision": "e28908f6a4aa3f0c8056",
+    "url": "/the-clap/static/js/2.e28908f6.chunk.js"
+  },
+  {
+    "revision": "66a1595ab2853de4fc22",
+    "url": "/the-clap/static/css/main.ea5b37e6.chunk.css"
+  },
+  {
+    "revision": "9e4f73e2b60b6eca62e2a531fdfbcbd4",
+    "url": "/the-clap/index.html"
+  }
+];
